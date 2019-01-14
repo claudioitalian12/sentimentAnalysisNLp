5a86710ae7aa6ea1568ea5f04fc6b04135bef06a
claudioitalian12 <dark.claudio12@gmail.com>
2019-01-14 22:14:16 +0100

    0.0.1

--- sentimentAnalysisNLp/ViewController.swift
+++ sentimentAnalysisNLp/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
-
+import NaturalLanguage
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+      
     }
 
 
+    
+    @IBAction func Results(_ sender: Any) {
+        
+        if((textField.text?.isEmpty)!){
+            return
+        }
+        
+        else {
+           
+            setImage()
+        }
+        
+    }
+    
+    func setImage(){
+        
+        let model = try! NLModel(contentsOf: TextClassifier.urlOfModelInThisBundle)
+        
+      let imageName =   model.predictedLabel(for:  textField.text!)!
+   
+        switch imageName {
+        case "positive":
+            image.image = UIImage(named: "positive")
+        case "negative":
+               image.image = UIImage(named: "negative")
+        case "neutral":
+               image.image = UIImage(named: "neutral")
+        default:
+            break
+        }
+        
+    }
 }
 
